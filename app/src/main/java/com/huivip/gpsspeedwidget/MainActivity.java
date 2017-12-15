b8e92fc63d87802edbbdb2e8181e36de6e7bb4bf
laisun <laisun@cisco.com>
2017-12-16 00:14:47 +0800

    forreimage

--- app/src/main/java/com/huivip/gpsspeedwidget/MainActivity.java
+++ app/src/main/java/com/huivip/gpsspeedwidget/MainActivity.java
@@ -3,12 +3,15 @@ package com.huivip.gpsspeedwidget;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -37,7 +40,7 @@ public class MainActivity extends Activity implements TraceListener {
     Calendar myCalendar = Calendar.getInstance();
     String selectDateStr="";
     AMap aMap=null;
-    String myFormat = "MM/dd/yyyy"; //In which you need put here
+    String myFormat = "MM/dd/yyyy";
     SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.CHINA);
     private int mSequenceLineID = 1000;
     private List<TraceLocation> mTraceList;
@@ -61,9 +64,68 @@ public class MainActivity extends Activity implements TraceListener {
         }
 
     };
+    public static void setSystemUiVisibility(Activity activity, boolean enterFullscreen) {
+        if (activity == null) {
+            return;
+        }
+        View decor = activity.getWindow().getDecorView();
+        if (enterFullscreen) {
+            activity.getWindow().setFlags(
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        } else {
+            activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+
+        /* place the window within the entire screen, ignoring
+         *  decorations around the border (such as the status bar).*/
+        activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
+        activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            return;
+        }
+        Window window = activity.getWindow();
+        // Translucent status bar
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        // Translucent navigation bar
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        int systemUiVisibility = decor.getSystemUiVisibility();
+        int flags = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        if (enterFullscreen) {
+            systemUiVisibility |= flags;
+        } else {
+            systemUiVisibility &= ~flags;
+        }
+        decor.setSystemUiVisibility(systemUiVisibility);
+    }
+    public static boolean isFullScreenActivity(Activity activity) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            return (activity.getWindow().getAttributes().flags & WindowManager.LayoutParams.FLAG_FULLSCREEN) > 0;
+        }
+        View decor = activity.getWindow().getDecorView();
+        int systemUiVisibility = decor.getSystemUiVisibility();
+        int flags = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        return (systemUiVisibility & flags) == flags;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         mMapView = (MapView) findViewById(R.id.map);
         mMapView.onCreate(savedInstanceState);
@@ -170,6 +232,7 @@ public class MainActivity extends Activity implements TraceListener {
             }
         };
         trackBtn.setOnClickListener(trackBtnListener);
+        setSystemUiVisibility(this,true);
     }
     private void drawLine(Message msg){
         List<LatLng> latLngs = new ArrayList<>();
