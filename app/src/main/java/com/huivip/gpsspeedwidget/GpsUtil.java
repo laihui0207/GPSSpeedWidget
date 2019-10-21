b72c546343f4e5f180db7911d58e6309cc374657
Laihui Sun (laisun) <laisun@cisco.com>
2019-10-22 01:20:58 +0800

    custom feature

--- app/src/main/java/com/huivip/gpsspeedwidget/GpsUtil.java
+++ app/src/main/java/com/huivip/gpsspeedwidget/GpsUtil.java
@@ -147,7 +147,7 @@ public class GpsUtil {
             }
         };
         Toast.makeText(context,"GPS服务开启",Toast.LENGTH_SHORT).show();
-        this.locationTimer.schedule(this.locationScanTask, 0L, 100L);
+        this.locationTimer.schedule(this.locationScanTask, 0L, 500L);
         /*Intent recordService = new Intent(context, RecordGpsHistoryService.class);
         context.startService(recordService);*/
         serviceStarted = true;
